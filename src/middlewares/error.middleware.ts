73c9023b825fd589d281c27a7332542617578e06
ProMustafa99 <mustafaksalameh99@gmail.com>
2024-12-16 16:02:33 +0300

    Make the ErrorMiddleware send Email

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,14 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@/exceptions/httpException';
 import { logger } from '@utils/logger';
+import { EmailService } from '@/services/ email.service';
+import Container from 'typedi';
+
 
 export const ErrorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
+
   try {
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
 
+    const emailService = Container.get(EmailService);
+    emailService.SendEmailForErrorApi(req, status, message);
+
     logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
     res.status(status).json({ message });
+
   } catch (error) {
     next(error);
   }
